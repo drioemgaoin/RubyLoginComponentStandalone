c6aaa76b492bee961b33bfdddd80ed345af81350
Diegoni <romain_diegoni@hotmail.com>
2017-01-05 12:46:45 +0000

    Update production configurationt to resolve heroku issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # General Settings
+  config.app_domain = 'localhost'
+
   # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
