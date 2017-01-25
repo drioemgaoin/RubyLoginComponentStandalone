3a67319f23e2be0b4e9a7dcfe4861913ef570827
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-25 21:33:08 +0000

    Fix issues when you display edit profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
-    if [:facebook, :google_oauth2].include? current_user.provider.to_sym
+    if current_user.provider.present? and [:facebook, :google_oauth2].include? current_user.provider.to_sym
       params.delete("current_password")
       resource.update_without_password(params)
     else
